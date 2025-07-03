3087de9055e71b77d54fb85f3de5aa57fe912284
desmo <desmond710v@gmail.com>
2025-07-03 18:41:34 +0500

    работа с входом и выходом

--- base/views.py
+++ base/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from .models import Task
+
 # Create your views here.
 
-def tsak(requests):
-    return HttpResponse('hello')
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    context_object_name = 'task_create'
+    template_name = 'base/task_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    context_object_name = 'task_update'
+    template_name = 'base/task_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task_delete'
+    template_name = 'base/task_delete.html'
+    success_url = reverse_lazy('tasks')
